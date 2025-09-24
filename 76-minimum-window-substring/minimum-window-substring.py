7baaeb85ab098634912adfe30940c1e6a03049bc
Ballem Nikitha <bnikitha9618@gmail.com>
2025-09-24 20:09:53 +0530

    Time: 2908 ms (5.00%) | Memory: 89.1 MB (11.60%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -0,0 +1,33 @@
+from collections import Counter
+
+class Solution(object):
+    def minWindow(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        s_count, t_count = Counter(), Counter(t)
+        
+        l, r = 0, 0
+        
+        results = []
+        
+        while r <= len(s)-1:
+                                
+            s_count[s[r]] += 1            
+            r += 1
+            if s_count & t_count != t_count:
+                continue
+                
+            while l < r:
+                s_count[s[l]] -= 1 
+                l += 1
+                if s_count & t_count == t_count:
+                    continue
+                results.append(s[l-1:r])
+                break
+            
+        if not results:
+            return ""        
+        return min(results, key=len)
